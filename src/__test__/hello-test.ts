9b1c91c04083f4b3f846dd3f460b4dffb0b1f834
卢林 <lulin666000@gmail.com>
2016-10-16 16:45:11 +0800

    up

---
+++ src/__test__/hello-test.ts
@@ -0,0 +1,10 @@
+import 'mocha';
+import hello from '../hello';
+import { expect } from 'chai';
+
+describe('Hello function', () => {
+  it('should return hello world', () => {
+    const result = hello();
+    expect(result).to.equal('Hello World!');
+  });
+});
